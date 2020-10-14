28113bd933d25da4b648d6e8c9ec7b3bfc4c3e74
camden9 <camdenvaughan@knights.ucf.edu>
2020-10-14 15:54:18 -0400

    Fixed another visual bug with the sky

--- Flappy Bird/Assets/Scripts/Menu Scripts/MenuBackgroundMovement.cs
+++ Flappy Bird/Assets/Scripts/Menu Scripts/MenuBackgroundMovement.cs
@@ -7,29 +7,44 @@ public class MenuBackgroundMovement : MonoBehaviour
     // Summary: Moves all of the background assets on the menu
     readonly float skyMoveSpeed = .3f;
     readonly float groundSpeed = 2.5f;
-    float timeTillSpawn;
-    readonly float targetTime = 2f;
+    float timeTillGroundSpawn;
+    float timeTillSkySpawn;
+    readonly float targetGroundTime = 2f;
+    readonly float targetSkyTime = 66.68f;
 
     private void Start()
     {
-        timeTillSpawn = 0f;
+        timeTillGroundSpawn = 0f;
     }
 
     void Update()
     {
         // Moves anything with the "Sky" tag
-        if (this.CompareTag("Sky")) transform.position = new Vector3(transform.position.x - skyMoveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        if (this.CompareTag("Sky"))
+        {
+            transform.position = new Vector3(transform.position.x - skyMoveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+
+            // Updates timer
+            timeTillSkySpawn += Time.deltaTime;
+
+            // If the timer is finished and the bird is alive the ground is moved
+            if (timeTillSkySpawn >= targetSkyTime)
+            {
+                transform.position = new Vector3(17f, 0f, 0f);
+                timeTillSkySpawn = 0f;
+            }
+        }
 
         // Moves anything with the "Ground" tag
         else if (this.CompareTag("Ground"))
         {
             transform.position = new Vector3(transform.position.x - groundSpeed * Time.deltaTime, transform.position.y, transform.position.z);
 
-            timeTillSpawn += Time.deltaTime;
-            if (timeTillSpawn >= targetTime)
+            timeTillGroundSpawn += Time.deltaTime;
+            if (timeTillGroundSpawn >= targetGroundTime)
             {
                 transform.position = new Vector3(3f, -4.5f, 0f);
-                timeTillSpawn = 0f;
+                timeTillGroundSpawn = 0f;
             }
         }
     }
---
+++ Flappy Bird/Assets/Scripts/SkyMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyMovement : MonoBehaviour
+{
+    // Variables for speed and timer, and refrence to BirdController
+    readonly float moveSpeed = 0.3f;
+    BirdController bird;
+    public float timeTillSpawn;
+    [SerializeField] float targetTime = 66.68f;
+
+
+    private void Awake()
+    {
+        // Gets current bird in the scene
+        bird = GameObject.FindGameObjectWithTag("Player").GetComponent<BirdController>();
+    }
+
+    private void Start()
+    {
+        // Resets timer
+        timeTillSpawn = 0f;
+    }
+
+    void Update()
+    {
+        if (bird.waitingForInput && Input.GetKeyDown(KeyCode.Space)) timeTillSpawn = 0f;
+        // Moves the ground if the bird is alive and gameplay has started
+        if (!bird.dead && !bird.waitingForInput) transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+
+        // Updates timer
+        timeTillSpawn += Time.deltaTime;
+
+        // If the timer is finished and the bird is alive the ground is moved
+        if (timeTillSpawn >= targetTime && !bird.dead)
+        {
+            transform.position = new Vector3(17f, 0f, 0f);
+            timeTillSpawn = 0f;
+        }
+    }
+}
